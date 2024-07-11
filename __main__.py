cf0361746cde60469cf2a54f3409f0b238e3f79d
Mythical <MythicalData@gmail.com>
2024-07-11 04:18:15 -0400

    Code cleanup

--- __main__.py
+++ __main__.py
@@ -2,59 +2,69 @@ import os
 import re
 import sys
 import json
-import time
 import shutil
 import requests
 import subprocess
 from zipfile import ZipFile
 from bs4 import BeautifulSoup
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
 
-settings_path = f"{script_dir}/settings.json"
+if getattr(sys, 'frozen', False):
+    script_dir = os.path.dirname(sys.executable)
+else:
+    script_dir  = os.path.dirname(os.path.abspath(__file__))
+
+
+settings_path = f'{script_dir}/settings.json'
+
 
 with open(settings_path, 'r') as file:
     settings = json.load(file)
-        
-archive_extractor_exe = f"{script_dir}/KFTempArchiveExtractor.exe"
-steam_cmd_dir = f"{script_dir}/steamcmd"
-steam_cmd_exe = f"{steam_cmd_dir}/steamcmd.exe"
-archive_dir = f"{script_dir}/KF Archive Files"
+
+
+archive_extractor_exe = f'{script_dir}/KFTempArchiveExtractor.exe'
+steam_cmd_dir = f'{script_dir}/steamcmd'
+steam_cmd_exe = f'{steam_cmd_dir}/steamcmd.exe'
+archive_dir = f'{script_dir}/KF Archive Files'
+
 
 steamcmd_url = 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip'
 
+
 def find_bin_files(root_path):
     bin_files = []
-    for foldername, subfolders, filenames in os.walk(root_path):
+    for foldername, filenames in os.walk(root_path):
         for filename in filenames:
-            if filename.endswith(".bin"):
+            if filename.endswith('.bin'):
                 bin_files.append(os.path.join(foldername, filename))
     return bin_files
 
+
 def find_files_in_directory(directory, file_types):
     found_files = []
-    for root, dirs, files in os.walk(directory):
+    for root, files in os.walk(directory):
         for file in files:
-            for file_type, extensions in file_types.items():
+            for extensions in file_types.items():
                 if file.lower().endswith(tuple(extensions)):
                     found_files.append(os.path.join(root, file))
                     break
     return found_files
+
 
 def move_files_to_directories(files, settings):
     for file_path in files:
         file_name = os.path.basename(file_path)
         file_type = None
 
-        for dir_name, extensions in settings["dir_names_to_file_types"].items():
+        for dir_name, extensions in settings['dir_names_to_file_types'].items():
             if file_name.lower().endswith(tuple(extensions)):
                 file_type = dir_name
                 break
 
         if file_type is not None:
             destination_dir = os.path.join(
-                settings["game_dir"],
-                settings["mods_dir"],
+                settings['game_dir'],
+                settings['mods_dir'],
                 file_type
             )
 
@@ -63,12 +73,13 @@ def move_files_to_directories(files, settings):
             destination_path = os.path.join(destination_dir, file_name)
 
             shutil.move(file_path, destination_path)
-            print(f"File '{file_name}' moved to '{destination_path}'")
+            print(f'File "{file_name}" moved to "{destination_path}"')
+
 
 def download_and_unzip_steamcmd():
     os.makedirs(steam_cmd_dir, exist_ok=True)
     response = requests.get(steamcmd_url)
-    zip_file_path = f"{steam_cmd_dir}/steamcmd.zip"
+    zip_file_path = f'{steam_cmd_dir}/steamcmd.zip'
 
     with open(zip_file_path, 'wb') as zip_file:
         zip_file.write(response.content)
@@ -77,8 +88,27 @@ def download_and_unzip_steamcmd():
         zip_ref.extractall(steam_cmd_dir)
         
     os.remove(zip_file_path)
+    
+    print('SteamCMD downloaded and unzipped successfully.')
+    os.chdir(steam_cmd_dir)
 
-def update_ini_file(game_dir, ini_path, paths_config):
+
+def update_ini_file():
+
+    with open(settings_path, 'r') as settings_file:
+        settings = json.load(settings_file)
+
+    game_dir = settings['game_dir']
+    ini_path = 'System/KillingFloor.ini'
+    mods_dir = settings['mods_dir']
+    dir_names_to_file_types = settings['dir_names_to_file_types']
+
+    paths_config = [
+        f'Paths={os.path.join("..", mods_dir, subdir)}/*{ext}'
+        for subdir, ext_list in dir_names_to_file_types.items()
+        for ext in ext_list
+    ]
+
     ini_full_path = os.path.join(game_dir, ini_path)
 
     with open(ini_full_path, 'r') as ini_file:
@@ -100,30 +130,11 @@ def update_ini_file(game_dir, ini_path, paths_config):
     with open(ini_full_path, 'w') as ini_file:
         ini_file.writelines(ini_content)
 
-def main():
-    with open(settings_path, 'r') as settings_file:
-        settings = json.load(settings_file)
 
-    game_dir = settings['game_dir']
-    ini_path = 'System/KillingFloor.ini'
-    mods_dir = settings['mods_dir']
-    dir_names_to_file_types = settings['dir_names_to_file_types']
-
-    paths_config = [
-        f'Paths={os.path.join("..", mods_dir, subdir)}/*{ext}'
-        for subdir, ext_list in dir_names_to_file_types.items()
-        for ext in ext_list
-    ]
-
-    update_ini_file(game_dir, ini_path, paths_config)
-    
-    with open(settings_path, 'r') as settings_file:
-        settings = json.load(settings_file)
+def get_subscription_ids():
+    all_subscription_app_ids = []
 
     urls = settings.get('workshop_collection_urls', [])
-
-
-    all_subscription_app_ids = []
 
     for url in urls:
         response = requests.get(url)
@@ -145,12 +156,10 @@ def main():
 
 
         all_subscription_app_ids.extend(subscription_app_ids)
+    return all_subscription_app_ids
 
-    
-    
-    os.chdir(steam_cmd_dir)
 
-    
+def get_current_commands():
     initial_commands = [
             steam_cmd_exe,
             "+login",
@@ -171,7 +180,7 @@ def main():
     current_length = sum(len(command) for command in initial_commands + end_commands)
     current_commands = initial_commands + end_commands
     
-    for mod_id in all_subscription_app_ids:
+    for mod_id in get_subscription_ids():
         workshop_command = "+workshop_download_item 1250 " + str(mod_id)
         if current_length + len(workshop_command) > max_command_length:
 
@@ -184,10 +193,14 @@ def main():
         middle_commands.append(workshop_command)
         current_commands.insert(-1, workshop_command)
         current_length += len(workshop_command)
-    
+    return current_commands
 
-    subprocess.run(current_commands)
-        
+
+def download_mod_archives():
+    subprocess.run(get_current_commands())
+
+
+def unpack_mod_archives():
     file_path = f"{steam_cmd_dir}/steamapps/workshop/content/1250"
     
     if not os.path.exists(file_path):
@@ -207,6 +220,8 @@ def main():
         print("Error: 'KF Archive Files' directory not found.")
         return
 
+
+def move_mod_files():
     files_to_search = settings["dir_names_to_file_types"]
     found_files = find_files_in_directory(archive_dir, files_to_search)
 
@@ -215,7 +230,14 @@ def main():
     else:
         print("No files found.")
 
-if __name__ == "__main__":
+
+def main():
     download_and_unzip_steamcmd()
-    print("SteamCMD downloaded and unzipped successfully.")
+    update_ini_file()
+    download_mod_archives()
+    unpack_mod_archives()  
+    move_mod_files()
+    
+
+if __name__ == "__main__":
     main()
