245440e4a919cc79ebf94a68e6b14e7c5d2faa51
Mythical-Github <MythicalData@gmail.com>
2024-11-07 15:26:59 -0500

    project cleanup and restructure

---
+++ src/kf1_mods_installer/cli.py
@@ -0,0 +1,11 @@
+from kf1_mods_installer import main
+
+OPTIONS = {
+    "module": main,
+    "commands": {
+        "download_and_install_mods": {
+            "function_name": "download_and_install_mods",
+            "arg_help_pairs": []
+        }
+    }
+}
